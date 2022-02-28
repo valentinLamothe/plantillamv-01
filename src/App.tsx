6acd45fb5075e552e5d873e94106c57382af00e7
unknown <valentinlamothecoulomme@hotmail.com>
2022-02-27 23:37:54 -0300

    first commit , the start

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div>Plantilla 01</div>
+      </div>
+  );
+}
+
+export default App;
